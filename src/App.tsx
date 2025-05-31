0682b77492ddd4be43cfa26d5ea3327a31e2bfd8
lufiDK4674 <133517851+lufiDK4674@users.noreply.github.com>
2025-06-01 02:54:57 +0530

    Fix: Blank

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/lufiDK4674.github.io">
+      <HashRouter>
         <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
           <Navbar />
           <Routes>
@@ -37,7 +37,7 @@ const App = () => (
           </Routes>
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
