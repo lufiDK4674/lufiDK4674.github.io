a8c644325108ead217c7a8610e1387387613de02
lufiDK4674 <133517851+lufiDK4674@users.noreply.github.com>
2025-06-01 03:01:19 +0530

    Fix: Blank

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/lufiDK4674.github.io/", // ✅ Correct for GitHub Pages root repo: lufiDK4674.github.io
+  base: "./", // ✅ Correct for GitHub Pages root repo: lufiDK4674.github.io
   plugins: [
     react(),
     mode === "development" && componentTagger(),
