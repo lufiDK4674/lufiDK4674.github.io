064a6a6deab04e7c866128a5748732556ca64770
lufiDK4674 <133517851+lufiDK4674@users.noreply.github.com>
2025-06-01 15:26:20 +0530

    deploy

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -46,7 +46,7 @@ const About = () => {
                     <img
                       src="https://i.postimg.cc/JnT9NyfL/Linked-In-Profile-UPSCALED.png"
                       alt="Divyanshu's Profile"
-                      className="w-64 h-64 object-cover"
+                      className="w-55 h-55 object-cover"
                     />
                   </div>
                 </div>
